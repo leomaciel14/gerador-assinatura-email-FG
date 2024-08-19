2fc4139b57e7fbd078982852bb23b0e2dab3b202
Leonardo Boeira from Windows <leonardoboeira.design@gmail.com>
2024-08-19 14:47:35 -0300

    atualização 2

--- generateEnv.js
+++ generateEnv.js
@@ -1,10 +1,14 @@
-const firebaseConfig = {
+const fs = require('fs');
+
+const envVars = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-export default firebaseConfig;
+const content = `window.env = ${JSON.stringify(envVars)};`;
+
+fs.writeFileSync('./public/env.js', content);
